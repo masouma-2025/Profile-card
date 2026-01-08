6ecefcb255890603384d01a8272f6c4e82ceb704
Masouma Najafi <masoumanajafi830@gmail.com>
2026-01-08 07:28:18 +0430

    part3

---
+++ src/components/AcountDeatiles.jsx
@@ -0,0 +1,13 @@
+export default function AcountDetiles({detalis}){
+    return(
+        <div className="card infoCard">
+            <h3 className="CardTitle">Acount Detalis</h3>
+
+            <div className="rows">
+                <Row label="Role" Value={detalis.Role}></Row>
+                <Row label="Status" Value={<span className="pill">Active</span>}></Row>
+                <Row label="Verification" Value={<span className="verified">Verified</span>}></Row>
+            </div>
+        </div>
+    )
+}
